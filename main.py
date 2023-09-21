d4f8e0d3eddbe4321a419270c575c9ab5d61ff0a
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-09-20 20:03:05 -0400

    Reformat

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import heapq
+'''
+Aiden Buterbaugh
+Thomas McLaughlin
+CS-255-01
 
-class Node:
+Project 1
+Due October 11, 2023
+
+Objective:
+    This program implements A*, to find the shortest path from a specified source node to a specified goal node. More specifically,
+    A* is applied to the real-world problem (RWP) of finding the shortest path to a user in a video game where edge weight indicates distance
+    between nodes/states.
+
+'''
+
+# Given: None
+# Task: Create a TreeNode class to represent nodes in the A* search algorithm.
+# Return: None
+class TreeNode(object):
     def __init__(self, position, parent=None):
-        self.position = position
-        self.parent = parent
+        self.position = position # Will be used once path found to list path
+        self.parent = parent # # Will be used once path found to list path
         self.g = 0  # Cost from start node to current node
         self.h = 0  # Heuristic (estimated cost from current node to goal node)
         self.f = 0  # Total cost (f = g + h)
