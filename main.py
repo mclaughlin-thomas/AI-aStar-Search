26f2dd6118ea94d70f895904819e182fa4b3e613
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-09-28 01:22:28 -0400

    Semi-linted using pylint

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ Project 1
 Due October 11, 2023
 
 Objective:
-    This program implements A*, to find the shortest path from a specified source node to a specified
-    goal node. More specifically here, A* is applied to the real-world problem (RWP) of finding the shortest path to a user
+    This program implements A*, to find the shortest path from a specified source node to
+    a specified goal node. More specifically here, A* is applied to the real-world problem (RWP)
+    of finding the shortest path to a user
     in a video game where edge weight indicates distance between nodes/states.
 '''
 import random
-           
-# Given: None
-# Task: Create nodes to be used in a graph
-# Return: None
+
+
 class Node(object):
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     def __init__(self, i, j):
         self.g = 0
         self.h = 0
@@ -26,8 +30,8 @@ class Node(object):
         self.neighbors = []
         self.previous = None
         self.wall = False
-        
-        if (random.randint(1, 100) < 10):
+
+        if random.randint(1, 100) < 10:
             self.wall = True
 
     def __addNeighbors__(self, grid,width, height):
@@ -50,13 +54,18 @@ class Node(object):
             self.neighbors.append(grid[i-1][j+1])
         if i < width-1 and j < height-1:
             self.neighbors.append(grid[i+1][j+1])
-        
+
 
 def setParameters():
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     try:
         width = int(input("Enter the width of the graph: "))
         height = int(input("Enter the height of the graph: "))
-        
+
         is_valid = True if width <= 0 or height <= 0 else False
         if is_valid is True:
             print("Number too small. Try again.\n\n")
@@ -68,6 +77,11 @@ def setParameters():
 
 
 def userGeneratedInput():
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     dummy_tuple:int = ()
     x = int(input())
     y = int(input())
@@ -76,6 +90,11 @@ def userGeneratedInput():
 
 
 def createSpace(width, height):
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     space = [0] * width
 
     for i in range(width): # entire space filled with 0's
@@ -93,31 +112,47 @@ def createSpace(width, height):
 
 
 def removeFromArray (array, element):
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     for i in range(len(array) - 1, -1, -1):
-        if (array[i] == element):
+        if array[i] == element:
             array.pop(i)
- 
+
 
 def heuristic (a,b):
-    # distance = math.dist((a.i, a.j), (b.i, b.j)) #euclidian distance, if ur feelin it ig| USE THIS IF U WANT TO ENABLE DIAGONAL
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
+    # distance = math.dist((a.i, a.j), (b.i, b.j)) #euclidian distance, if ur feelin it ig
+    # USE THIS IF U WANT TO ENABLE DIAGONAL
     distance = abs(a.i-b.i) + abs(a.j-b.j)
     return distance
 
 
-def astar(startSpace, endSpace):
+def astar(start_space, end_space):
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     path = []
     open_set = []
     closed_set = []
 
 
-    open_set.append(startSpace)
-    
-    
+    open_set.append(start_space)
+
+
     while open_set:  # This loop will continue as long as openSet is not empty
         winner = 0
 
         for i in range(len(open_set)):
-            if(open_set[i].f < open_set[winner].f):
+            if open_set[i].f < open_set[winner].f:
                 winner = i
 
         current = open_set[winner]
@@ -125,7 +160,7 @@ def astar(startSpace, endSpace):
         print(current.f," ",current.h," ",current.g," ")###
         print("\n")
 
-        if current == endSpace:
+        if current == end_space:
             # if we find it, relay path
             path = []
             temp = current
@@ -133,7 +168,7 @@ def astar(startSpace, endSpace):
             while temp.previous:
                 path.append(temp.previous)
                 temp = temp.previous
-            print("\n Done")  
+            print("\n Done")
             # Print the coordinates of the node
             return path
         removeFromArray(open_set, current)
@@ -144,23 +179,24 @@ def astar(startSpace, endSpace):
             #g value increases one going to each square
             neighbor = neighbors[i]
 
-            # little search inside a search
-            if neighbor not in closed_set and not neighbor.wall: # IF ITS A NEIGHBOR thats not in the closed set
-                tempG = current.g +1 #
-                
-                newPath = False
-                if (neighbor in open_set):
-                    if(tempG<neighbor.g):
-                        neighbor.g = tempG
-                        newPath = True
+            # little search inside a search del me later
+            # IF ITS A NEIGHBOR thats not in the closed set del me later
+            if neighbor not in closed_set and not neighbor.wall:
+                temp_g = current.g +1 #
+
+                new_path = False
+                if neighbor in open_set:
+                    if temp_g<neighbor.g:
+                        neighbor.g = temp_g
+                        new_path = True
                 else:
-                    neighbor.g = tempG
-                    newPath = True
+                    neighbor.g = temp_g
+                    new_path = True
                     open_set.append(neighbor)
-                
+
                 #heuristics
-                if newPath:
-                    neighbor.h = heuristic(neighbor, endSpace)
+                if new_path:
+                    neighbor.h = heuristic(neighbor, end_space)
                     neighbor.f = neighbor.g + neighbor.h
                     neighbor.previous = current
 
@@ -169,19 +205,24 @@ def astar(startSpace, endSpace):
 
 
 def main():
+    """
+    Given: None
+    Task: Create nodes to be used in a graph
+    Return: None
+    """
     width, height = setParameters()
     space = createSpace(width, height)
-    startSpace = space[0][0]
-    endSpace = space[width-1][height-1]
-    startSpace.wall = False
-    endSpace.wall = False
-    #endSpace = space[0][4]
-    path = astar(startSpace, endSpace)
-   
+    start_space = space[0][0]
+    end_space = space[width-1][height-1]
+    start_space.wall = False
+    end_space.wall = False
+    #end_space = space[0][4]
+    path = astar(start_space, end_space)
+
     if path:
         print("Path:")
         for node in reversed(path):
-            print(f"({node.i}, {node.j})") 
+            print(f"({node.i}, {node.j})")
     else:
         print("No path found")
 
